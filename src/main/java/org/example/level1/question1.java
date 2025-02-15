c5cf1fd5e420b214a989c68ee53b8ed1cfed5f2f
sunmo <ksm0212000@naver.com>
2025-02-15 18:06:52 +0900

    # 코딩테스트 - 약수의 합

--- src/main/java/org/example/level1/question1.java
+++ src/main/java/org/example/level1/question1.java
@@ -13,6 +13,7 @@ public class question1 {
          * Integer은 int를 감싸는 클래스이다.
          * int는 원시 타입 이지만 Integer은 객체타입이다.
          *
+         *
          */
         //parseInt는 문자열을 int 타입으로 변환해준다.
 

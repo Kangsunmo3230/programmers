17d97960700de91c6ab8b8504a5149cbd38a9bab
sunmo <ksm0212000@naver.com>
2025-02-23 23:16:25 +0900

    # 코딩테스트 -  test

--- src/main/java/org/example/level1/question5.java
+++ src/main/java/org/example/level1/question5.java
@@ -22,6 +22,7 @@ public class question5 {
 
         //제곱근
 
+
     }
 
 
---
+++ src/main/java/org/example/level1/question6.java
@@ -0,0 +1,14 @@
+package org.example.level1;
+
+/**
+ * 정수 제곱근 판별
+ */
+public class question6 {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}

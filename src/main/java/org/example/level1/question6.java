21337cf6721a9ee7f758ac1486567fe510ef575c
sunmo <ksm0212000@naver.com>
2025-03-18 22:37:51 +0900

    # 코딩테스트 -  정수 제곱근 판별

--- src/main/java/org/example/level1/question6.java
+++ src/main/java/org/example/level1/question6.java
@@ -5,9 +5,17 @@ package org.example.level1;
  */
 public class question6 {
     public static void main(String[] args) {
+        long n = 3;
+        long result = -1;
+        // 1. 제곱근 구하기
+        double value = Math.sqrt(n);
+        // 2. 다시 제곱으로 곱해서 해당 값이 n 인지 판별
+        Math.pow((int) value, 2);
+        System.out.println(value);
 
-        //2.27
-
+        if (Math.pow((int) Math.sqrt(n), 2) == n) {
+            result =  (long) Math.pow(Math.sqrt(n) + 1, 2);
+        }
     }
 
 
